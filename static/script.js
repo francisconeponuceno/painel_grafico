a69602100f3b66ba21bca46428652783b87c7a05
francisconeponuceno <120109535+francisconeponuceno@users.noreply.github.com>
2024-07-17 12:58:57 -0300

    atualizacao

--- static/script.js
+++ static/script.js
@@ -10,32 +10,59 @@ let porcentoClaudino = document.getElementById('porcentoClaudino');
 let porcentoEscoamento = document.getElementById('porcentoEscoamento');
 let Cladino_Escoamento = Number(porcentoClaudino.innerHTML) + Number(porcentoEscoamento.innerHTML)
 
-const ctx = document.getElementById('grafico1');
-const ctx2 = document.getElementById('grafico2');
 
-  new Chart(ctx, {
-    type: 'doughnut',
-    data: {
-      labels: ['Terceiro', 'Claudino', 'Escoamento','Escoamento + Claudino' ],
-      datasets: [{
-        label: '%',
-        
-        data: [Number(porcentoTerceiro.innerHTML), Number(porcentoClaudino.innerHTML),
-              Number(porcentoEscoamento.innerHTML), Number(Cladino_Escoamento)],
-        backgroundColor: ['#ff0000', '#0000ff', '#4881fce6','#f86f6f']
-        
-      }]
-    },
-    options: {
-      layout : {
-        with : 300
-      }
-
+// PRIMEIRO GRAFICO
+let ctx = document.getElementById('grafico1');
+new Chart(ctx, {
+  type: 'doughnut',
+  data: {
+    labels: ['Terceiro', 'Claudino', 'Escoamento','Escoamento + Claudino' ],
+    datasets: [{
+      label: '%',
+      
+      data: [Number(porcentoTerceiro.innerHTML), Number(porcentoClaudino.innerHTML),
+            Number(porcentoEscoamento.innerHTML), Number(Cladino_Escoamento)],
+      backgroundColor: ['#ff0000', '#0000ff', '#4881fce6','#f86f6f']
+      
+    }]
+  },
+  options: {
+    layout : {
+      with : 300
     }
-  });
+
+  }
+});
  
+
 // SEGUNDO GRAFICO
+let ctx2 = document.getElementById('grafico2');
 new Chart(ctx2, {
+  type: 'line',
+  data: {
+    labels: ['Arimatéia', 'Fabio', 'Zé carlos','Cazé','Kassio','Vicente'],
+    datasets: [{
+      label: 'CUB',
+      
+      data: [Number(cubArimateia.innerHTML),Number(cubFabio.innerHTML),
+            Number(cubZecarlos.innerHTML),Number(cubCaze.innerHTML),
+            Number(cubKassio.innerHTML),Number(cubVicente.innerHTML)],
+      backgroundColor: ['#0000ff','#ff0000']
+      
+    }]
+  },
+  options: {
+    layout : {
+      width : 100
+    }
+
+  }
+});
+
+
+// TERCEIRO GRAFICO
+let ctx3 = document.getElementById('grafico3');
+new Chart(ctx3, {
   type: 'bar',
   data: {
     labels: ['Arimatéia', 'Fabio', 'Zé carlos','Cazé','Kassio','Vicente'],
