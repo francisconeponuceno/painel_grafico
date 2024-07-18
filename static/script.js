6addfe1b9a5180265fa5beef00fc66acd5de0c8f
francisconeponuceno <120109535+francisconeponuceno@users.noreply.github.com>
2024-07-18 14:25:17 -0300

    atualizacao

--- static/script.js
+++ static/script.js
@@ -43,7 +43,7 @@ new Chart(ctx2, {
   data: {
     labels: [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30,31],
     datasets: [{
-      label: 'CUB',
+      label: '',
       
       data: [Number(cubArimateia.innerHTML),Number(cubFabio.innerHTML),
             Number(cubZecarlos.innerHTML),Number(cubCaze.innerHTML),
