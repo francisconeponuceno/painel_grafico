941e1987bec87ce5a657f7fe811ae636d7de12fe
FRANCISCO NEPONUCENO <120109535+francisconeponuceno@users.noreply.github.com>
2024-07-24 22:53:48 -0300

    atualizacao

--- main.py
+++ main.py
@@ -54,6 +54,6 @@ def cadastrar():
 
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(host="127.0.0.1", port=5000, debug=True)
 
 # app.run(host='127.0.0.1', port=5000, debug=True)
