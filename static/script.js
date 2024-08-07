8bf5a0815bf2ab553cc6c3ed3bc1a383b4b1a5a4
francisconeponuceno <120109535+francisconeponuceno@users.noreply.github.com>
2024-08-07 09:11:12 -0300

    ATUALIZACAO

--- static/script.js
+++ static/script.js
@@ -65,6 +65,7 @@ new Chart('chart', {
 
 
 // SEGUNDO GRAFICO
+/*
 let ctx2 = document.getElementById('grafico2');
 new Chart(ctx2, {
   type: 'bar',
@@ -86,6 +87,262 @@ new Chart(ctx2, {
     
   }
 });
+*/
+
+// GRAFICO 2
+am5.ready(function() {
+
+  // Data labels: 
+  var allData = {
+    "2002": {
+      "Arimatéia": 100,
+      "Fabio": 90,
+      "Zé carlos": 80,
+      "Cazé": 70,
+      "Kassio": 60,
+      "Vicente": 50,
+      "Fernando": 40,
+      "Raione": 30,
+      "Lucas": 20
+    }
+  };
+  
+  
+  // Create root element
+  // https://www.amcharts.com/docs/v5/getting-started/#Root_element
+  var root = am5.Root.new("chartdiv2");
+  
+  root.numberFormatter.setAll({
+    numberFormat: "#a",
+  
+    // Group only into M (millions), and B (billions)
+    bigNumberPrefixes: [
+      { number: 1e6, suffix: "M" },
+      { number: 1e9, suffix: "B" }
+    ],
+  
+    // Do not use small number prefixes at all
+    smallNumberPrefixes: []
+  });
+  
+  var stepDuration = 2000;
+  
+  
+  // Set themes
+  // https://www.amcharts.com/docs/v5/concepts/themes/
+  root.setThemes([am5themes_Animated.new(root)]);
+  
+  
+  // Create chart
+  // https://www.amcharts.com/docs/v5/charts/xy-chart/
+  var chart = root.container.children.push(am5xy.XYChart.new(root, {
+    panX: true,
+    panY: true,
+    wheelX: "none",
+    wheelY: "none",
+    paddingLeft: 0
+  }));
+  
+  
+  // We don't want zoom-out button to appear while animating, so we hide it at all
+  chart.zoomOutButton.set("forceHidden", true);
+  
+  
+  // Create axes
+  // https://www.amcharts.com/docs/v5/charts/xy-chart/axes/
+  var yRenderer = am5xy.AxisRendererY.new(root, {
+    minGridDistance: 20,
+    inversed: true,
+    minorGridEnabled: true
+  });
+  // hide grid
+  yRenderer.grid.template.set("visible", false);
+  
+  var yAxis = chart.yAxes.push(am5xy.CategoryAxis.new(root, {
+    maxDeviation: 0,
+    categoryField: "network",
+    renderer: yRenderer
+  }));
+  
+  var xAxis = chart.xAxes.push(am5xy.ValueAxis.new(root, {
+    maxDeviation: 0,
+    min: 0,
+    strictMinMax: true,
+    extraMax: 0.1,
+    renderer: am5xy.AxisRendererX.new(root, {})
+  }));
+  
+  xAxis.set("interpolationDuration", stepDuration / 10);
+  xAxis.set("interpolationEasing", am5.ease.linear);
+  
+  
+  // Add series
+  // https://www.amcharts.com/docs/v5/charts/xy-chart/series/
+  var series = chart.series.push(am5xy.ColumnSeries.new(root, {
+    xAxis: xAxis,
+    yAxis: yAxis,
+    valueXField: "value",
+    categoryYField: "network"
+  }));
+  
+  // Rounded corners for columns
+  series.columns.template.setAll({ cornerRadiusBR: 5, cornerRadiusTR: 5 });
+  
+  // Make each column to be of a different color
+  series.columns.template.adapters.add("fill", function (fill, target) {
+    return chart.get("colors").getIndex(series.columns.indexOf(target));
+  });
+  
+  series.columns.template.adapters.add("stroke", function (stroke, target) {
+    return chart.get("colors").getIndex(series.columns.indexOf(target));
+  });
+  
+  // Add label bullet
+  series.bullets.push(function () {
+    return am5.Bullet.new(root, {
+      locationX: 1,
+      sprite: am5.Label.new(root, {
+        text: "{valueXWorking.formatNumber('#.# a')}",
+        fill: root.interfaceColors.get("alternativeText"),
+        centerX: am5.p100,
+        centerY: am5.p50,
+        populateText: true
+      })
+    });
+  });
+  
+  var label = chart.plotContainer.children.push(am5.Label.new(root, {
+    text: "2002",
+    fontSize: "8em",
+    opacity: 0.2,
+    x: am5.p100,
+    y: am5.p100,
+    centerY: am5.p100,
+    centerX: am5.p100
+  }));
+  
+  // Get series item by category
+  function getSeriesItem(category) {
+    for (var i = 0; i < series.dataItems.length; i++) {
+      var dataItem = series.dataItems[i];
+      if (dataItem.get("categoryY") == category) {
+        return dataItem;
+      }
+    }
+  }
+  
+  // Axis sorting
+  function sortCategoryAxis() {
+    // sort by value
+    series.dataItems.sort(function (x, y) {
+      return y.get("valueX") - x.get("valueX"); // descending
+      //return x.get("valueX") - y.get("valueX"); // ascending
+    });
+  
+    // go through each axis item
+    am5.array.each(yAxis.dataItems, function (dataItem) {
+      // get corresponding series item
+      var seriesDataItem = getSeriesItem(dataItem.get("category"));
+  
+      if (seriesDataItem) {
+        // get index of series data item
+        var index = series.dataItems.indexOf(seriesDataItem);
+        // calculate delta position
+        var deltaPosition =
+          (index - dataItem.get("index", 0)) / series.dataItems.length;
+        // set index to be the same as series data item index
+        if (dataItem.get("index") != index) {
+          dataItem.set("index", index);
+          // set deltaPosition instanlty
+          dataItem.set("deltaPosition", -deltaPosition);
+          // animate delta position to 0
+          dataItem.animate({
+            key: "deltaPosition",
+            to: 0,
+            duration: stepDuration / 2,
+            easing: am5.ease.out(am5.ease.cubic)
+          });
+        }
+      }
+    });
+    // sort axis items by index.
+    // This changes the order instantly, but as deltaPosition is set, they keep in the same places and then animate to true positions.
+    yAxis.dataItems.sort(function (x, y) {
+      return x.get("index") - y.get("index");
+    });
+  }
+  
+  var year = 2002;
+  
+  // update data with values each 1.5 sec
+  var interval = setInterval(function () {
+    year++;
+  
+    if (year > 2018) {
+      clearInterval(interval);
+      clearInterval(sortInterval);
+    }
+  
+    updateData();
+  }, stepDuration);
+  
+  var sortInterval = setInterval(function () {
+    sortCategoryAxis();
+  }, 100);
+  
+  function setInitialData() {
+    var d = allData[year];
+  
+    for (var n in d) {
+      series.data.push({ network: n, value: d[n] });
+      yAxis.data.push({ network: n });
+    }
+  }
+  
+  function updateData() {
+    var itemsWithNonZero = 0;
+  
+    if (allData[year]) {
+      label.set("text", year.toString());
+  
+      am5.array.each(series.dataItems, function (dataItem) {
+        var category = dataItem.get("categoryY");
+        var value = allData[year][category];
+  
+        if (value > 0) {
+          itemsWithNonZero++;
+        }
+  
+        dataItem.animate({
+          key: "valueX",
+          to: value,
+          duration: stepDuration,
+          easing: am5.ease.linear
+        });
+        dataItem.animate({
+          key: "valueXWorking",
+          to: value,
+          duration: stepDuration,
+          easing: am5.ease.linear
+        });
+      });
+  
+      yAxis.zoom(0, itemsWithNonZero / yAxis.dataItems.length);
+    }
+  }
+  
+  setInitialData();
+  setTimeout(function () {
+    year++;
+    updateData();
+  }, 50);
+  
+  // Make stuff animate on load
+  // https://www.amcharts.com/docs/v5/concepts/animations/
+  series.appear(1000);
+  chart.appear(1000, 100);
+  
+  }); // end am5.ready()
 
 
 // TERCEIRO GRAFICO
@@ -114,7 +371,9 @@ new Chart(ctx3, {
 });
 
 */
-am5.ready(function() {
+
+// GRAFICO 3
+am5.ready(function() { 
 
 
   // Create root element
@@ -146,7 +405,7 @@ am5.ready(function() {
   // Create axes
   // https://www.amcharts.com/docs/v5/charts/xy-chart/axes/
   var xRenderer = am5xy.AxisRendererX.new(root, {
-    minGridDistance: 30,
+    minGridDistance: 25,
     minorGridEnabled: true
   });
   xRenderer.labels.template.setAll({
@@ -182,8 +441,8 @@ am5.ready(function() {
   
   // Rounded corners for columns
   series.columns.template.setAll({
-    cornerRadiusTL: 5,
-    cornerRadiusTR: 5,
+    cornerRadiusTL: 0,
+    cornerRadiusTR: 0,
     strokeOpacity: 0
   });
   
@@ -205,7 +464,7 @@ am5.ready(function() {
       sprite: am5.Label.new(root, {
         text: "{valueYWorking.formatNumber('#.')}",
         fill: root.interfaceColors.get("alternativeText"),
-        centerY: 0,
+        centerY: 8,
         centerX: am5.p50,
         populateText: true
       })
@@ -213,43 +472,43 @@ am5.ready(function() {
   });
   
   
-  // Set data
+  // Set data 
   var data = [{
     "country": "Jan",
-    "value": 70
+    "value": 80 //Number(jan)
   }, {
     "country": "Fev",
-    "value": 80
+    "value": 80 //Number(fev)
   }, {
     "country": "Mar",
-    "value": 80
+    "value": 80 //Number(mar)
   }, {
     "country": "Abr",
-    "value": 80
+    "value": 80 //Number(abr)
   }, {
     "country": "Mai",
-    "value": 80
+    "value": 80 //Number(mai)
   }, {
     "country": "Jun",
-    "value": 80
+    "value": 80 //Number(jun)
   }, {
     "country": "Jul",
-    "value": 80
+    "value": 80 //Number(jul)
   }, {
     "country": "Ago",
-    "value": 80
+    "value": 80 //Number(ago)
   }, {
     "country": "Set",
-    "value": 80
+    "value": 80 //Number(set)
   }, {
     "country": "Out",
-    "value": 80
+    "value": 80 //Number(out)
   }, {
     "country": "Nov",
-    "value": 80
+    "value": 80 //Number(nov)
   }, {
     "country": "Dez",
-    "value": 80
+    "value": 80 //Number(dez)
   }];
   
 
