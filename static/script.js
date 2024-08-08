d40b5880147cf2b4e98e341511814f797d6fe9b2
francisconeponuceno <120109535+francisconeponuceno@users.noreply.github.com>
2024-08-08 17:34:40 -0300

    atualizacao

--- static/script.js
+++ static/script.js
@@ -77,263 +77,109 @@ pieSeries.labels.template.adapter.add("text", function(text, target) {
 
 
 
+/*
+"Arimatéia": Number(cubArimateia.innerHTML),
+"Fabio": Number(cubFabio.innerHTML),
+"Zé carlos": Number(cubZecarlos.innerHTML),
+"Cazé": Number(cubCaze.innerHTML),
+"Kassio": Number(cubKassio.innerHTML),
+"Vicente": Number(cubVicente.innerHTML),
+"Fernando": Number(cubFernando.innerHTML),
+"Raione": Number(cubRaione.innerHTML),
+"Lucas": Number(cubLucas.innerHTML)
+*/
+
 
 // GRAFICO 2
-am5.ready(function() {
+am4core.ready(function() {
 
-  // Data labels: 
-  var allData = {
-    "2002": {
-      "Arimatéia": Number(cubArimateia.innerHTML),
-      "Fabio": Number(cubFabio.innerHTML),
-      "Zé carlos": Number(cubZecarlos.innerHTML),
-      "Cazé": Number(cubCaze.innerHTML),
-      "Kassio": Number(cubKassio.innerHTML),
-      "Vicente": Number(cubVicente.innerHTML),
-      "Fernando": Number(cubFernando.innerHTML),
-      "Raione": Number(cubRaione.innerHTML),
-      "Lucas": Number(cubLucas.innerHTML)
-    }
-  };
+  // Themes begin
+  am4core.useTheme(am4themes_animated);
+  // Themes end
   
+  var chart = am4core.create("chartdiv2", am4charts.XYChart);
+  chart.padding(5, 15, 0, 0);
   
-  // Create root element
-  // https://www.amcharts.com/docs/v5/getting-started/#Root_element
-  var root = am5.Root.new("chartdiv2");
+  var categoryAxis = chart.yAxes.push(new am4charts.CategoryAxis());
+  categoryAxis.renderer.grid.template.location = 0;
+  categoryAxis.dataFields.category = "network";
+  categoryAxis.renderer.minGridDistance = 1;
+  categoryAxis.renderer.inversed = true;
+  categoryAxis.renderer.grid.template.disabled = true;
   
-  root.numberFormatter.setAll({
-    numberFormat: "#a",
+  var valueAxis = chart.xAxes.push(new am4charts.ValueAxis());
+  valueAxis.min = 0;
   
-    // Group only into M (millions), and B (billions)
-    bigNumberPrefixes: [
-      { number: 1e6, suffix: "M" },
-      { number: 1e9, suffix: "B" }
-    ],
+  var series = chart.series.push(new am4charts.ColumnSeries());
+  series.dataFields.categoryY = "network";
+  series.dataFields.valueX = "MAU";
+  series.tooltipText = "{valueX.value}"
+  series.columns.template.strokeOpacity = 0;
+  series.columns.template.column.cornerRadiusBottomRight = 0;
+  series.columns.template.column.cornerRadiusTopRight = 0;
+  series.columns.template.propertyFields.fill = "color"; // Associa a cor definida no objeto de dados
   
-    // Do not use small number prefixes at all
-    smallNumberPrefixes: []
-  });
+  var labelBullet = series.bullets.push(new am4charts.LabelBullet())
+  labelBullet.label.horizontalCenter = "left";
+  labelBullet.label.dx = 10;
+  labelBullet.label.text = "{values.valueX.workingValue.formatNumber('#.0as')}";
+  labelBullet.locationX = 1;
   
-  var stepDuration = 2000;
+  // as by default columns of the same series are of the same color, we add adapter which takes colors from chart.colors color set
+  //series.columns.template.adapter.add("fill", function(fill, target){
+    //return chart.colors.getIndex(target.dataItem.index);
+  //});
   
-  
-  // Set themes
-  // https://www.amcharts.com/docs/v5/concepts/themes/
-  root.setThemes([am5themes_Animated.new(root)]);
-  
-  
-  // Create chart
-  // https://www.amcharts.com/docs/v5/charts/xy-chart/
-  var chart = root.container.children.push(am5xy.XYChart.new(root, {
-    panX: true,
-    panY: true,
-    wheelX: "none",
-    wheelY: "none",
-    paddingLeft: 5,
-    paddingBottom: 0,
-    paddingTop: 3
-    
-  }));
-  
-  
-  // We don't want zoom-out button to appear while animating, so we hide it at all
-  chart.zoomOutButton.set("forceHidden", true);
-  
-  
-  // Create axes
-  // https://www.amcharts.com/docs/v5/charts/xy-chart/axes/
-  var yRenderer = am5xy.AxisRendererY.new(root, {
-    minGridDistance: 10,
-    inversed: true,
-    minorGridEnabled: true
-  });
-  // hide grid
-  yRenderer.grid.template.set("visible", false);
-  
-  var yAxis = chart.yAxes.push(am5xy.CategoryAxis.new(root, {
-    maxDeviation: 0,
-    categoryField: "network",
-    renderer: yRenderer
-  }));
-  
-  var xAxis = chart.xAxes.push(am5xy.ValueAxis.new(root, {
-    maxDeviation: 0,
-    min: 0,
-    strictMinMax: true,
-    extraMax: 0.1,
-    renderer: am5xy.AxisRendererX.new(root, {})
-  }));
-  
-  xAxis.set("interpolationDuration", stepDuration / 10);
-  xAxis.set("interpolationEasing", am5.ease.linear);
-  
-  
-  // Add series
-  // https://www.amcharts.com/docs/v5/charts/xy-chart/series/
-  var series = chart.series.push(am5xy.ColumnSeries.new(root, {
-    xAxis: xAxis,
-    yAxis: yAxis,
-    valueXField: "value",
-    categoryYField: "network"
-  }));
-  
-  // Rounded corners for columns
-  series.columns.template.setAll({ cornerRadiusBR: 0, cornerRadiusTR: 0 });
-  
-  // Make each column to be of a different color
-  series.columns.template.adapters.add("fill", function (fill, target) {
-    return chart.get("colors").getIndex(series.columns.indexOf(target));
-  });
-  
-  series.columns.template.adapters.add("stroke", function (stroke, target) {
-    return chart.get("colors").getIndex(series.columns.indexOf(target));
-  });
-  
-  // Add label bullet
-  series.bullets.push(function () {
-    return am5.Bullet.new(root, {
-      locationX: 1,
-      sprite: am5.Label.new(root, {
-        text: "{valueXWorking.formatNumber('#.# a')}",
-        fill: root.interfaceColors.get("alternativeText"),
-        centerX: am5.p100,
-        centerY: am5.p50,
-        populateText: true
-      })
-    });
-  });
-  
-  var label = chart.plotContainer.children.push(am5.Label.new(root, {
-    text: "",
-    fontSize: "8em",
-    opacity: 0.2,
-    x: am5.p100,
-    y: am5.p100,
-    centerY: am5.p100,
-    centerX: am5.p100
-  }));
-  
-  // Get series item by category
-  function getSeriesItem(category) {
-    for (var i = 0; i < series.dataItems.length; i++) {
-      var dataItem = series.dataItems[i];
-      if (dataItem.get("categoryY") == category) {
-        return dataItem;
+  categoryAxis.sortBySeries = series;
+  chart.data = [
+      {
+        "network": "Arimatéia",
+        "MAU": Number(cubArimateia.innerHTML),
+        "color": am4core.color("#0000ff") // AZUL
+      },
+      {
+        "network": "Fabio",
+        "MAU": Number(cubFabio.innerHTML),
+        "color": am4core.color("#ff0000") // VERMELHO
+      },
+      {
+        "network": "Zé carlos",
+        "MAU": Number(cubZecarlos.innerHTML),
+        "color": am4core.color("#0000ff") // AZUL
+      },
+      {
+        "network": "Cazé",
+        "MAU": Number(cubCaze.innerHTML),
+        "color": am4core.color("#ff0000") // VERMELHO
+      },
+      {
+        "network": "Kassio",
+        "MAU": Number(cubKassio.innerHTML),
+        "color": am4core.color("#0000ff") // AZUL
+      },
+      {
+        "network": "Vicente",
+        "MAU": Number(cubVicente.innerHTML),
+        "color": am4core.color("#ff0000") // VERMELHO
+      },
+      {
+        "network": "Fernando",
+        "MAU": Number(cubFernando.innerHTML),
+        "color": am4core.color("#0000ff") // AZUL
+      },
+      {
+        "network": "Raione",
+        "MAU": Number(cubRaione.innerHTML),
+        "color": am4core.color("#ff0000") // VERMELHO
+      },
+      {
+        "network": "Lucas",
+        "MAU": Number(cubLucas.innerHTML),
+        "color": am4core.color("#0000ff") // AZUL
       }
-    }
-  }
+    ]
   
-  // Axis sorting
-  function sortCategoryAxis() {
-    // sort by value
-    series.dataItems.sort(function (x, y) {
-      return y.get("valueX") - x.get("valueX"); // descending
-      //return x.get("valueX") - y.get("valueX"); // ascending
-    });
-  
-    // go through each axis item
-    am5.array.each(yAxis.dataItems, function (dataItem) {
-      // get corresponding series item
-      var seriesDataItem = getSeriesItem(dataItem.get("category"));
-  
-      if (seriesDataItem) {
-        // get index of series data item
-        var index = series.dataItems.indexOf(seriesDataItem);
-        // calculate delta position
-        var deltaPosition =
-          (index - dataItem.get("index", 0)) / series.dataItems.length;
-        // set index to be the same as series data item index
-        if (dataItem.get("index") != index) {
-          dataItem.set("index", index);
-          // set deltaPosition instanlty
-          dataItem.set("deltaPosition", -deltaPosition);
-          // animate delta position to 0
-          dataItem.animate({
-            key: "deltaPosition",
-            to: 0,
-            duration: stepDuration / 2,
-            easing: am5.ease.out(am5.ease.cubic)
-          });
-        }
-      }
-    });
-    // sort axis items by index.
-    // This changes the order instantly, but as deltaPosition is set, they keep in the same places and then animate to true positions.
-    yAxis.dataItems.sort(function (x, y) {
-      return x.get("index") - y.get("index");
-    });
-  }
-  
-  var year = 2002;
-  
-  // update data with values each 1.5 sec
-  var interval = setInterval(function () {
-    year++;
-  
-    if (year > 2018) {
-      clearInterval(interval);
-      clearInterval(sortInterval);
-    }
-  
-    updateData();
-  }, stepDuration);
-  
-  var sortInterval = setInterval(function () {
-    sortCategoryAxis();
-  }, 100);
-  
-  function setInitialData() {
-    var d = allData[year];
-  
-    for (var n in d) {
-      series.data.push({ network: n, value: d[n] });
-      yAxis.data.push({ network: n });
-    }
-  }
-  
-  function updateData() {
-    var itemsWithNonZero = 0;
-  
-    if (allData[year]) {
-      label.set("text", year.toString());
-  
-      am5.array.each(series.dataItems, function (dataItem) {
-        var category = dataItem.get("categoryY");
-        var value = allData[year][category];
-  
-        if (value > 0) {
-          itemsWithNonZero++;
-        }
-  
-        dataItem.animate({
-          key: "valueX",
-          to: value,
-          duration: stepDuration,
-          easing: am5.ease.linear
-        });
-        dataItem.animate({
-          key: "valueXWorking",
-          to: value,
-          duration: stepDuration,
-          easing: am5.ease.linear
-        });
-      });
-  
-      yAxis.zoom(0, itemsWithNonZero / yAxis.dataItems.length);
-    }
-  }
-  
-  setInitialData();
-  setTimeout(function () {
-    year++;
-    updateData();
-  }, 50);
-  
-  // Make stuff animate on load
-  // https://www.amcharts.com/docs/v5/concepts/animations/
-  series.appear(1000);
-  chart.appear(1000, 100);
-  }); // end am5.ready()
+  }); // end am4core.ready()
 
 
 
