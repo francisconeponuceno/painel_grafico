a9c37dd90ec70ee7c93c7fbdcc93168e2dca5a4f
FRANCISCO NEPONUCENO <120109535+francisconeponuceno@users.noreply.github.com>
2025-10-15 13:26:10 -0300

    ATUALIZACAO

--- main.py
+++ main.py
@@ -72,4 +72,4 @@ def remover(id):
 if __name__ == '__main__':
     app.run(host="127.0.0.1", port=5000, debug=True)
 
-# app.run(host='127.0.0.1', port=5000, debug=True)
+# app.run(host='0.0.0.0', port= 'A DEFINIR', debug=True)
